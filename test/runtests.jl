1ccefca29956a5a57dfc51c5a109fafb24508c7a
Fons van der Plas <fons@plutojl.org>
2021-05-27 22:05:14 +0200

    add test

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+using Test
+
+using PlutoExport
+
+
+testfile = download("https://raw.githubusercontent.com/fonsp/Pluto.jl/main/sample/Tower%20of%20Hanoi.jl")
+
+
+outfile = @test_nowarn pluto_to_pdf(testfile; open=false)
+
+@test isfile(outfile)
+@test endswith(outfile, ".pdf")
+
