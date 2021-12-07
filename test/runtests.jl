92f2516a5cc2cf6f4587634a5a22f8e715e90d03
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-07 11:42:35 +0100

    Upload test output as artifact

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,19 @@ using Test
 using PlutoPDF
 
 
-testfile = download("https://raw.githubusercontent.com/fonsp/Pluto.jl/main/sample/Tower%20of%20Hanoi.jl")
+testfile = download(
+    "https://raw.githubusercontent.com/fonsp/Pluto.jl/main/sample/Tower%20of%20Hanoi.jl",
+)
 
 
+is_CI = get(ENV, "CI", "no") == "no"
 
-outfile = pluto_to_pdf(testfile; open=get(ENV, "CI", "no") == "no")
+outfile = pluto_to_pdf(testfile; open=is_CI)
 
 @test isfile(outfile)
 @test endswith(outfile, ".pdf")
+
+output_dir = get(ENV, "TEST_OUTPUT_DIR", nothing)
+if output_dir isa String
+    cp(outfile, joinpath(output_dir, "hanoi.pdf"))
+end
