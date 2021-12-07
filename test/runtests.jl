c002a71e6b04c3a53a46c5bedc550a1de53cdb23
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-07 11:58:15 +0100

    asdf

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,12 +13,13 @@ is_CI = get(ENV, "CI", "no") == "no"
 
 outfile = pluto_to_pdf(testfile; open=is_CI)
 
-@show pwd()
 
 @test isfile(outfile)
 @test endswith(outfile, ".pdf")
 
 output_dir = get(ENV, "TEST_OUTPUT_DIR", nothing)
+@info "huh" readdir(pwd()) isdir(output_dir)
+
 if output_dir isa String
     cp(outfile, joinpath(output_dir, "hanoi.pdf"))
 end
