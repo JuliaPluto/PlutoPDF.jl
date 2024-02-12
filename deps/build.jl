a549b00eb99563c0c4479ce7000c93c70c37c4ee
Fons van der Plas <fonsvdplas@gmail.com>
2024-02-12 11:16:47 +0100

    whitespace

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,5 @@
 import NodeJS
 npm = NodeJS.npm_cmd()
 node_root = normpath(joinpath(@__DIR__, "../node"))
+run(`$npm --version`)
 println(run(`$npm --prefix=$node_root --scripts-prepend-node-path=true install $node_root`))
--- src/PlutoPDF.jl
+++ src/PlutoPDF.jl
@@ -10,18 +10,18 @@ export html_to_pdf, pluto_to_pdf, pdf
 # Stolen from https://discourse.julialang.org/t/collecting-all-output-from-shell-commands/15592
 "Run a Cmd object, returning the stdout & stderr contents plus the exit code"
 function execute(cmd::Cmd)
-  out = Pipe()
-  err = Pipe()
+    out = Pipe()
+    err = Pipe()
 
-  process = run(pipeline(ignorestatus(cmd), stdout=out, stderr=err))
-  close(out.in)
-  close(err.in)
+    process = run(pipeline(ignorestatus(cmd), stdout=out, stderr=err))
+    close(out.in)
+    close(err.in)
 
-  (
-    stdout = String(read(out)), 
-    stderr = String(read(err)),  
-    code = process.exitcode
-  )
+    (
+        stdout = String(read(out)), 
+        stderr = String(read(err)),  
+        code = process.exitcode,
+    )
 end
 
 const default_options = (
@@ -36,7 +36,11 @@ const default_options = (
     displayHeaderFooter=false,
 )
 
-function html_to_pdf(html_path::AbstractString, output_path::Union{AbstractString,Nothing}=nothing; options=default_options, open=true, console_output=true)
+function html_to_pdf(html_path::AbstractString, output_path::Union{AbstractString,Nothing}=nothing; 
+    options=default_options, 
+    open=true, 
+    console_output=true
+)
     bin_script = normpath(joinpath(@__DIR__, "../node/bin.js"))
 
     output_path = something(output_path, Pluto.numbered_until_new(splitext(html_path)[1]; suffix=".pdf", create_file=false))
@@ -63,11 +67,11 @@ function pluto_to_pdf(notebook_path::AbstractString, output_path::Union{Abstract
         disable_writing_notebook_files = true,
         lazy_workspace_creation = true,
     )
-    s = Pluto.ServerSession(;options=c)
+    session = Pluto.ServerSession(;options=c)
     @info "Running notebook..."
-    nb = Pluto.SessionActions.open(s, notebook_path; run_async=false)
-    html_contents = Pluto.generate_html(nb; binder_url_js="undefined")
-    Pluto.SessionActions.shutdown(s, nb)
+    notebook = Pluto.SessionActions.open(session, notebook_path; run_async=false)
+    html_contents = Pluto.generate_html(notebook; binder_url_js="undefined")
+    Pluto.SessionActions.shutdown(session, notebook)
 
     filename = tempname() * ".html"
     write(filename, html_contents)
