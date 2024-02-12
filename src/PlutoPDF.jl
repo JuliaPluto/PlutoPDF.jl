f851777d0d8220f7eeb85f3204e22159660c8b54
Fons van der Plas <fonsvdplas@gmail.com>
2024-02-12 11:23:39 +0100

    Document pluto_to_pdf

--- src/PlutoPDF.jl
+++ src/PlutoPDF.jl
@@ -63,7 +63,20 @@ function html_to_pdf(html_path::AbstractString, output_path::Union{AbstractStrin
     output_path
 end
 
+"""
+```julia
+pluto_to_pdf(notebook_path::String[, output_pdf_path::String]; 
+    options=default_options,
+    open::Bool=true,
+    console_output::Bool=true,
+)
+```
 
+Run a notebook, generate an Export HTML and then print it to a PDF file!
+
+# Options
+The `options` keyword argument can be a named tuple to configure the PDF export. The possible options can be seen in the [docs for `puppeteer.PDFOptions`](https://pptr.dev/api/puppeteer.pdfoptions).
+"""
 function pluto_to_pdf(notebook_path::AbstractString, output_path::Union{AbstractString,Nothing}=nothing; kwargs...)
     c = Pluto.Configuration.from_flat_kwargs(;
         disable_writing_notebook_files = true,
