42285cf71ac2674d0ad695cd284b48c1789cda74
Fons van der Plas <fonsvdplas@gmail.com>
2024-02-12 13:02:17 +0100

    ughhhh

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,7 @@
 import NodeJS_18_jll: npm
+
+@assert isfile(npm)
+
 node_root = normpath(joinpath(@__DIR__, "../node"))
 run(`$npm --version`)
 println(run(`$npm --prefix=$node_root --scripts-prepend-node-path=true install $node_root`))
