3ace873745a19a601347e01cab9e8b4baae8f438
Fons van der Plas <fonsvdplas@gmail.com>
2024-02-12 13:23:46 +0100

    add warning about windows not fully supported

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,11 @@ import NodeJS_18_jll: npm
 
 @assert isfile(npm)
 
+
+if Sys.iswindows()
+    @warn "Windows might not be supported because of https://github.com/JuliaPackaging/Yggdrasil/issues/8095"
+end
+
 node_root = normpath(joinpath(@__DIR__, "../node"))
 run(`$npm --version`)
 println(run(`$npm --prefix=$node_root --scripts-prepend-node-path=true install $node_root`))
--- src/PlutoPDF.jl
+++ src/PlutoPDF.jl
@@ -96,6 +96,12 @@ function pluto_to_pdf(notebook_path::AbstractString, output_path::Union{Abstract
     html_to_pdf(filename, output_path; kwargs...)
 end
 
+function __init__()
+    if Sys.iswindows()
+        @warn "Windows might not be supported because of https://github.com/JuliaPackaging/Yggdrasil/issues/8095"
+    end
+end
+
 @deprecate pdf(args...; kwargs...) pluto_to_pdf(args...; kwargs...)
 
 end
