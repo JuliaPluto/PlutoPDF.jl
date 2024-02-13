d6dba2e6c493bc53c7350efbb27801fef84c3e20
Fons van der Plas <fonsvdplas@gmail.com>
2024-02-13 12:41:18 +0100

    how about this
    Co-Authored-By: Παναγιώτης Γεωργακόπουλος <pankgeorg@gmail.com>

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,6 @@
 import NodeJS_18_jll
-
-const npm = if Sys.iswindows()
-    NodeJS_18_jll.npm.cmd
-else
-    NodeJS_18_jll.npm
-end
+const npm = Sys.iswindows() ? "$(NodeJS_18_jll.npm).cmd" : NodeJS_18_jll.npm
 
 node_root = normpath(joinpath(@__DIR__, "../node"))
 run(`$npm --version`)
-println(run(`$npm --prefix=$node_root --scripts-prepend-node-path=true install $node_root`))
+run(`$npm --prefix=$node_root --scripts-prepend-node-path=true install $node_root`)
