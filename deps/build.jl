d3d64bbfaadec25c4721bd4b71866c74ce6808cb
Fons van der Plas <fonsvdplas@gmail.com>
2024-02-13 12:48:57 +0100

    Update build.jl
    Co-Authored-By: Παναγιώτης Γεωργακόπουλος <pankgeorg@gmail.com>

--- deps/build.jl
+++ deps/build.jl
@@ -2,5 +2,7 @@ import NodeJS_18_jll
 const npm = Sys.iswindows() ? "$(NodeJS_18_jll.npm).cmd" : NodeJS_18_jll.npm
 
 node_root = joinpath(dirname(@__DIR__), "node")
-run(`$npm --version`)
-run(`$npm --prefix=$node_root --scripts-prepend-node-path=true install $node_root`)
+cd(node_root) do
+    run(`$npm --version`)
+    run(`$npm install`)
+end
