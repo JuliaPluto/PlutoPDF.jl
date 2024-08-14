56116ddc06064531f292c372626c2188d31cc345
Fons van der Plas <fonsvdplas@gmail.com>
2024-08-14 23:02:11 +0200

    asdf

--- src/setup_build.jl
+++ src/setup_build.jl
@@ -24,25 +24,34 @@ function npm_cmd()
 end
 
 function get_build_dir()
-    build_node(@get_scratch!("build_dir3"))
+    build_node(@get_scratch!("build_dir4"))
 end
 
+const node_build_lock = ReentrantLock()
 
 function build_node(dir)
-    @info "PlutoPDF: Running npm install in scratch space..."
-    
-    if dir !== dirname(node_root_files[1])
-        for f in node_root_files
-            readwrite(f, joinpath(dir, basename(f)))
+    lock(node_build_lock) do
+        @info "PlutoPDF: Running npm install in scratch space..."
+        
+        if dir !== dirname(node_root_files[1])
+            for f in node_root_files
+                readwrite(f, joinpath(dir, basename(f)))
+            end
         end
+        
+        npm_cache_dir = @get_scratch!("npm_cache")
+        
+        cmd = addenv(npm_cmd(), "NPM_CONFIG_CACHE" => npm_cache_dir)
+        
+        @info "huh" cmd
+        
+        cd(dir) do
+            run(`$(cmd) --version`)
+            run(`$(cmd) install --audit-level=none --no-fund --no-audit`)
+        end
+        
+        dir
     end
-    
-    cd(dir) do
-        run(`$(npm_cmd()) --version`)
-        run(`$(npm_cmd()) install --audit-level=none --no-fund --no-audit`)
-    end
-    
-    dir
 end
 
 "Like `cp` except we create the file manually (to fix permission issues). (It's not plagiarism if you use this function to copy homework.)"
