cafbb7a74355510bda0defeb3aba7b7152f9ae82
Fons van der Plas <fonsvdplas@gmail.com>
2025-01-30 14:50:37 +0100

    Use nodejs path list (#19)

--- src/setup_build.jl
+++ src/setup_build.jl
@@ -9,34 +9,46 @@ const node_root_files = (
     @path(joinpath(dirname(@__DIR__), "node", "package.json")),
 )
 
-const npm = !Sys.iswindows() ? NodeJS_20_jll.npm : let
-    new = "$(NodeJS_20_jll.npm).cmd"
-    isfile(new) ? new : NodeJS_20_jll.npm
+
+function npm_cmd()
+    npm = !Sys.iswindows() ? NodeJS_20_jll.npm : let
+        new = "$(NodeJS_20_jll.npm).cmd"
+        isfile(new) ? new : NodeJS_20_jll.npm
+    end
+    
+    # Add NodeJS PATH to the existing PATH environment variable 
+    path_sep = Sys.iswindows() ? ';' : ':'
+    path_list = prepend!(split(get(ENV, "PATH", ""), path_sep), NodeJS_20_jll.PATH_list)
+    path = join(path_list, path_sep)
+    addenv(`$(npm)`, "PATH" => path)
 end
 
 function get_build_dir()
     build_node(@get_scratch!("build_dir4"))
 end
 
-ci() = get(ENV, "CI", "neetjes") != "neetjes"
+const node_build_lock = ReentrantLock()
 
 function build_node(dir)
-    @info "PlutoPDF: Running npm install in scratch space..."
-    
-    if dir !== dirname(node_root_files[1])
-        for f in node_root_files
-            readwrite(f, joinpath(dir, basename(f)))
+    lock(node_build_lock) do
+        npm_cache_dir = @get_scratch!("npm_cache")
+        cmd = addenv(npm_cmd(), "NPM_CONFIG_CACHE" => npm_cache_dir)
+
+        @info "PlutoPDF: Running npm install in scratch space..." npm_version=Text(read(`$(cmd) --version`, String))
+
+        if dir != dirname(node_root_files[1])
+            for f in node_root_files
+                readwrite(f, joinpath(dir, basename(f)))
+            end
         end
+        
+        cd(dir) do
+            run(`$(cmd) install --audit-level=none --no-fund --no-audit`)
+        end
+        
+        @info "PlutoPDF: Finished npm install."
+        dir
     end
-    
-    cd(dir) do
-        run(`$npm --version`)
-        run(ci() ? `$npm install --verbose` : `$npm install`)
-    end
-    
-    @info "PlutoPDF: Finished npm install."
-    
-    dir
 end
 
 "Like `cp` except we create the file manually (to fix permission issues). (It's not plagiarism if you use this function to copy homework.)"
